411b7826dc595380d70135e1f0d9c4cb5e64c378
Claus Anderwald <claus.anderwald@gebit.de>
2021-02-22 10:18:43 +0100

    RPA-116: created show case to demonstrate PubSubRef gRPC problem

--- grpc-example/grpc-example-java/hello-impl/src/main/java/com/example/hello/impl/HelloGrpcServiceImpl.java
+++ grpc-example/grpc-example-java/hello-impl/src/main/java/com/example/hello/impl/HelloGrpcServiceImpl.java
@@ -1,30 +1,84 @@
 package com.example.hello.impl;
 
-import akka.actor.ActorSystem;
-import akka.stream.Materializer;
-import example.myapp.helloworld.grpc.AbstractGreeterServiceRouter;
-import example.myapp.helloworld.grpc.HelloReply;
-import example.myapp.helloworld.grpc.HelloRequest;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionStage;
+
+import akka.NotUsed;
+import akka.actor.ActorSystem;
+import akka.stream.Materializer;
+import akka.stream.javadsl.Source;
+import example.myapp.helloworld.grpc.AbstractGreeterServiceRouter;
+import example.myapp.helloworld.grpc.HelloReply;
+import example.myapp.helloworld.grpc.HelloRequest;
+import example.myapp.helloworld.grpc.StatusRequest;
+import example.myapp.helloworld.grpc.StatusResponse;
+import scala.concurrent.ExecutionContext;
 
 @Singleton
 public class HelloGrpcServiceImpl extends AbstractGreeterServiceRouter {
 
-    @Inject
-    public HelloGrpcServiceImpl(ActorSystem sys, Materializer mat) {
-        super(mat, sys);
-    }
+	/**
+	 * Actor System.
+	 */
+	private ActorSystem actorSystem = null;
 
-    @Override
-    public CompletionStage<HelloReply> sayHello(HelloRequest in) {
-        HelloReply reply = HelloReply
-            .newBuilder()
-            .setMessage("Hi " + in.getName() + " (gRPC)")
-            .build();
-        return CompletableFuture.completedFuture(reply);
-    }
+	/**
+	 * ExecutionContext.
+	 */
+	private ExecutionContext executionContext = null;
+
+	/**
+	 * GrpcAppTopic.
+	 */
+	@Inject
+	private GrpcStatusTopic grpcStatusTopic;
+
+	/**
+	 * Boolean flag of status tick source has been started.
+	 */
+	private static boolean statusTickStarted = false;
+
+	@Inject
+	public HelloGrpcServiceImpl(ActorSystem sys, Materializer mat, ExecutionContext anExecutionContext) {
+		super(mat, sys);
+		actorSystem = sys;
+		executionContext = anExecutionContext;
+	}
+
+	@Override
+	public CompletionStage<HelloReply> sayHello(HelloRequest in) {
+		HelloReply reply = HelloReply.newBuilder().setMessage("Hi " + in.getName() + " (gRPC)").build();
+		return CompletableFuture.completedFuture(reply);
+	}
+
+	@Override
+	public Source<StatusResponse, NotUsed> statusStream(StatusRequest aIn) {
+		System.out.println("statusStream invoked");
+		if (!statusTickStarted) {
+			System.out.println("starting scheduler");
+			actorSystem.scheduler()
+					.scheduleWithFixedDelay(Duration.ofSeconds(5),
+							// setting the following value to 90 will cause the connection error problem at the clients
+							Duration.ofSeconds(5),
+							this::publishStatus,
+							executionContext);
+
+			statusTickStarted = true;
+			System.out.println("scheduler started");
+		}
+
+		Source<StatusResponse, NotUsed> tPubSubSubscriber = grpcStatusTopic.subscriber();
+		return tPubSubSubscriber;
+	}
+
+	public void publishStatus() {
+		System.out.println("publishStatus invoked");
+		grpcStatusTopic.publish(Instant.now().toString());
+	}
+
 }
--- grpc-example/grpc-example-java/hello-impl/src/main/java/com/example/hello/impl/HelloModule.java
+++ grpc-example/grpc-example-java/hello-impl/src/main/java/com/example/hello/impl/HelloModule.java
@@ -5,12 +5,11 @@ import com.google.inject.AbstractModule;
 import com.lightbend.lagom.javadsl.server.ServiceGuiceSupport;
 
 public class HelloModule extends AbstractModule implements ServiceGuiceSupport {
-    @Override
-    protected void configure() {
-        bindService(
-            HelloService.class, HelloServiceImpl.class,
-            additionalRouter(HelloGrpcServiceImpl.class)
-        );
-    }
+
+	@Override
+	protected void configure() {
+		bindService(HelloService.class, HelloServiceImpl.class, additionalRouter(HelloGrpcServiceImpl.class));
+		bind(GrpcStatusTopic.class).asEagerSingleton();
+	}
 
 }
