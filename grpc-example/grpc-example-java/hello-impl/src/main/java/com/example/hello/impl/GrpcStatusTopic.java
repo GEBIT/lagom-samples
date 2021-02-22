6595ec0fe9db90e04ce16126461973493ca90799
Claus Anderwald <claus.anderwald@gebit.de>
2021-02-22 10:18:43 +0100

    RPA-102: created show case to demonstrate gRPC connection problems

---
+++ grpc-example/grpc-example-java/hello-impl/src/main/java/com/example/hello/impl/GrpcStatusTopic.java
@@ -0,0 +1,86 @@
+//
+// GrpcAppTopic.java
+//
+// Copyright (C) 2020
+// GEBIT Solutions GmbH,
+// Berlin, Duesseldorf, Stuttgart (Germany)
+// All rights reserved.
+//
+package com.example.hello.impl;
+
+import java.util.concurrent.CompletionStage;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.lightbend.lagom.javadsl.pubsub.PubSubRef;
+import com.lightbend.lagom.javadsl.pubsub.PubSubRegistry;
+import com.lightbend.lagom.javadsl.pubsub.TopicId;
+
+import akka.Done;
+import akka.NotUsed;
+import akka.stream.javadsl.Source;
+import example.myapp.helloworld.grpc.StatusResponse;
+
+/**
+ * GrpcAppTopic implementation.
+ * 
+ * @author CAnderwald
+ */
+public class GrpcStatusTopic {
+
+	/**
+	 * The logger.
+	 */
+	private static final Logger LOGGER = LoggerFactory.getLogger(GrpcStatusTopic.class);
+
+	/**
+	 * Qualifier for the topic.
+	 */
+	private static final String TOPIC_QUALIFIER = "topic-global";
+
+	/**
+	 * the lagom pub sub registry.
+	 */
+	private final PubSubRegistry pubSub;
+
+	/**
+	 * C' tor.
+	 * 
+	 * @param aPubSub the pubsub registry
+	 */
+	@Inject
+	public GrpcStatusTopic(PubSubRegistry aPubSub) {
+		this.pubSub = aPubSub;
+	}
+
+	/**
+	 * Provide a subscription for an checkout receipt.
+	 */
+	public Source<StatusResponse, NotUsed> subscriber() {
+		Source<StatusResponse, NotUsed> tSource = refFor().subscriber();
+		return tSource;
+	}
+
+	/**
+	 * Publish a status.
+	 */
+	public CompletionStage<Done> publish(String aStatus) {
+		PubSubRef<StatusResponse> tRef = refFor();
+
+		return tRef.hasAnySubscribers().thenApply(hasAnySubscribers -> {
+			if (!hasAnySubscribers) {
+				LOGGER.info("no active subscriptions");
+			}
+			tRef.publish(StatusResponse.newBuilder().setStatus(aStatus).build());
+			return Done.getInstance();
+		});
+	}
+
+	private PubSubRef<StatusResponse> refFor() {
+		return pubSub.refFor(TopicId.of(StatusResponse.class, TOPIC_QUALIFIER));
+	}
+
+}
--- grpc-example/grpc-example-java/hello-proxy-impl/src/test/java/com/example/helloproxy/impl/HelloProxyServiceImplTest.java
+++ grpc-example/grpc-example-java/hello-proxy-impl/src/test/java/com/example/helloproxy/impl/HelloProxyServiceImplTest.java
@@ -1,101 +1,111 @@
 package com.example.helloproxy.impl;
 
-import akka.Done;
-import akka.NotUsed;
-import akka.grpc.javadsl.AkkaGrpcClient;
-import com.example.hello.api.HelloService;
-import com.example.helloproxy.api.HelloProxyService;
-import com.lightbend.lagom.javadsl.api.ServiceCall;
-import com.lightbend.lagom.javadsl.testkit.ServiceTest;
-import example.myapp.helloworld.grpc.AkkaGrpcClientModule;
-import example.myapp.helloworld.grpc.GreeterServiceClient;
-import example.myapp.helloworld.grpc.HelloReply;
-import example.myapp.helloworld.grpc.HelloRequest;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import static com.lightbend.lagom.javadsl.testkit.ServiceTest.bind;
+import static com.lightbend.lagom.javadsl.testkit.ServiceTest.defaultSetup;
+import static com.lightbend.lagom.javadsl.testkit.ServiceTest.startServer;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.junit.Assert.assertEquals;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
-import static com.lightbend.lagom.javadsl.testkit.ServiceTest.*;
-import static java.util.concurrent.TimeUnit.SECONDS;
-import static org.junit.Assert.assertEquals;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.example.hello.api.HelloService;
+import com.example.helloproxy.api.HelloProxyService;
+import com.lightbend.lagom.javadsl.api.ServiceCall;
+import com.lightbend.lagom.javadsl.testkit.ServiceTest;
+
+import akka.Done;
+import akka.NotUsed;
+import akka.grpc.javadsl.AkkaGrpcClient;
+import akka.stream.javadsl.Source;
+import example.myapp.helloworld.grpc.AkkaGrpcClientModule;
+import example.myapp.helloworld.grpc.GreeterServiceClient;
+import example.myapp.helloworld.grpc.HelloReply;
+import example.myapp.helloworld.grpc.HelloRequest;
+import example.myapp.helloworld.grpc.StatusRequest;
+import example.myapp.helloworld.grpc.StatusResponse;
 
 public class HelloProxyServiceImplTest {
 
-    private static ServiceTest.TestServer server;
-    private static    HelloProxyService proxyServiceClient ;
+	private static ServiceTest.TestServer server;
 
-    @BeforeClass
-    public static void setUp() {
-        ServiceTest.Setup setup = defaultSetup()
-            .configureBuilder(builder ->
-                builder
-                    .disable(AkkaGrpcClientModule.class)
-                    .overrides(bind(HelloService.class).to(StubHelloService.class))
-                    .overrides(bind(GreeterServiceClient.class).to(GreeterServiceClientStub.class))
-            );
-        server = startServer(setup);
-        proxyServiceClient = server.client(HelloProxyService.class);
-    }
+	private static HelloProxyService proxyServiceClient;
 
-    @AfterClass
-    public static void tearDown() {
-        if (server != null) {
-            server.stop();
-            server = null;
-        }
-    }
+	@BeforeClass
+	public static void setUp() {
+		ServiceTest.Setup setup = defaultSetup().configureBuilder(builder -> builder.disable(AkkaGrpcClientModule.class)
+				.overrides(bind(HelloService.class).to(StubHelloService.class))
+				.overrides(bind(GreeterServiceClient.class).to(GreeterServiceClientStub.class)));
+		server = startServer(setup);
+		proxyServiceClient = server.client(HelloProxyService.class);
+	}
 
-    @Test
-    public void helloProxyShouldRoundtripHttpRequests() throws InterruptedException, ExecutionException, TimeoutException {
-        String msg = proxyServiceClient.proxyViaHttp("Alice").invoke()
-            .toCompletableFuture().get(5, SECONDS);
-        assertEquals("Hello Alice", msg);
-    }
-    @Test
-    public void helloProxyShouldRoundtripGrpcRequests() throws InterruptedException, ExecutionException, TimeoutException {
-        String msg = proxyServiceClient.proxyViaGrpc("Steve").invoke()
-            .toCompletableFuture().get(5, SECONDS);
-        assertEquals("Hi Steve (gRPC)", msg);
-    }
+	@AfterClass
+	public static void tearDown() {
+		if (server != null) {
+			server.stop();
+			server = null;
+		}
+	}
 
-    // ---------------------------------------------------------------------------------
+	@Test
+	public void helloProxyShouldRoundtripHttpRequests()
+			throws InterruptedException, ExecutionException, TimeoutException {
+		String msg = proxyServiceClient.proxyViaHttp("Alice").invoke().toCompletableFuture().get(5, SECONDS);
+		assertEquals("Hello Alice", msg);
+	}
 
-    public static class StubHelloService implements HelloService {
-        @Override
-        public ServiceCall<NotUsed, String> hello(String id) {
-            return notUsed -> CompletableFuture.completedFuture("Hello " + id);
-        }
-    }
+	@Test
+	public void helloProxyShouldRoundtripGrpcRequests()
+			throws InterruptedException, ExecutionException, TimeoutException {
+		String msg = proxyServiceClient.proxyViaGrpc("Steve").invoke().toCompletableFuture().get(5, SECONDS);
+		assertEquals("Hi Steve (gRPC)", msg);
+	}
 
+	// ---------------------------------------------------------------------------------
 
-    public static class GreeterServiceClientStub extends GreeterServiceClient implements StubbedAkkaGrpcClient {
-        @Override
-        public CompletionStage<HelloReply> sayHello(HelloRequest in) {
-            HelloReply reply = HelloReply.newBuilder()
-                .setMessage("Hi " + in.getName() + " (gRPC)")
-                .build();
-            return CompletableFuture.completedFuture(reply);
-        }
-    }
+	public static class StubHelloService implements HelloService {
 
-// ------------------------------------------------------------------------
-// ------------------------------------------------------------------------
+		@Override
+		public ServiceCall<NotUsed, String> hello(String id) {
+			return notUsed -> CompletableFuture.completedFuture("Hello " + id);
+		}
+	}
 
-    public interface StubbedAkkaGrpcClient extends AkkaGrpcClient{
-        @Override
-        default CompletionStage<Done> close() {
-            return null;
-        }
+	public static class GreeterServiceClientStub extends GreeterServiceClient implements StubbedAkkaGrpcClient {
 
-        @Override
-        default CompletionStage<Done> closed() {
-            return null;
-        }
-    }
+		@Override
+		public CompletionStage<HelloReply> sayHello(HelloRequest in) {
+			HelloReply reply = HelloReply.newBuilder().setMessage("Hi " + in.getName() + " (gRPC)").build();
+			return CompletableFuture.completedFuture(reply);
+		}
+
+		@Override
+		public Source<StatusResponse, NotUsed> statusStream(StatusRequest aIn) {
+			return null;
+		}
+	}
+
+	// ------------------------------------------------------------------------
+	// ------------------------------------------------------------------------
+
+	public interface StubbedAkkaGrpcClient extends AkkaGrpcClient {
+
+		@Override
+		default CompletionStage<Done> close() {
+			return null;
+		}
+
+		@Override
+		default CompletionStage<Done> closed() {
+			return null;
+		}
+	}
 
 }
